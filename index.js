924a11927d1b0e61096d96f2a322a1f19b54d13c
xMaveriqi <saverorrebaixada@gmail.com>
2025-11-20 16:05:03 +0000

    fiz umas paradas ae

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ let form = document.querySelector(".form");
 let calc = "";
 let result = "";
 let htmlResult = document.querySelector(".htmlResult");
-form.addEventListener("submit", (event)=>{
+form.addEventListener("submit", (event) => {
     event.preventDefault()
 
     let submitter = event.submitter.name
-    if(submitter == "="){
+    if (submitter == "=") {
         result = eval(calc);
-    htmlResult.innerHTML = `${result}`
-}else if {
-   
-}else {
+        htmlResult.innerHTML = `${result}`
+    } else if (submitter == "c") {
+        result = "";
+        calc = "";
+        htmlResult.innerHTML = `0`
+    }
 
- calc = calc + submitter;
-    htmlResult.innerHTML = `${calc}`
 
-}
+
+
+    else {
+        calc = calc + submitter;
+        htmlResult.innerHTML = `${calc}`
+    }
+
 
 })
